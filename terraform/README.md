7d6318407eecb657792c7606c28d6bfa26f37729
Victor Morales <victor.morales@intel.com>
2020-01-02 10:18:51 -0800

    Add terraform installation script

---
+++ terraform/README.md
@@ -0,0 +1,11 @@
+# Terraform
+
+Terraform is an open-source infrastructure as code software tool
+created by HashiCorp. It enables users to define and provision a
+datacenter infrastructure using a high-level configuration language
+known as Hashicorp Configuration Language (HCL), or optionally JSON.
+Terraform supports a number of cloud infrastructure providers such as
+Amazon Web Services, IBM Cloud (formerly Bluemix), Google Cloud
+Platform, DigitalOcean, Linode, Microsoft Azure, Oracle Cloud
+Infrastructure, OVH, or VMware vSphere as well as OpenNebula and
+OpenStack. 
---
+++ terraform/main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2020
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function main {
+    local version=${PKG_TERRAFORM_VERSION:-0.12.18}
+    local tarball="terraform_${version}_linux_amd64.zip"
+
+    if command -v terraform; then
+        return
+    fi
+
+    pushd "$(mktemp -d)"
+    curl -o "$tarball" "https://releases.hashicorp.com/terraform/$version/$tarball"
+    if ! command -v unzip; then
+        curl -fsSL http://bit.ly/pkgInstall | PKG=unzip bash
+    fi
+    unzip "$tarball"
+    sudo mkdir -p /usr/local/bin/
+    sudo mv terraform /usr/local/bin/
+    rm "$tarball"
+    mkdir -p ~/.terraform.d/plugins
+    popd
+}
+
+main
