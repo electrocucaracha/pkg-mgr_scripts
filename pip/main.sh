e3a49b69cb0daed19cfc30bd21e8b24d5448f0d1
Victor Morales <victor.morales@intel.com>
2020-01-22 10:47:25 -0800

    Add python3 support to pip installation

--- pip/main.sh
+++ pip/main.sh
@@ -12,28 +12,78 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-function main {
-    if command -v pip; then
-        return
-    fi
+# _vercmp() - Function that compares two versions
+function _vercmp {
+    local v1=$1
+    local op=$2
+    local v2=$3
+    local result
 
-    if ! command -v python; then
+    # sort the two numbers with sort's "-V" argument.  Based on if v2
+    # swapped places with v1, we can determine ordering.
+    result=$(echo -e "$v1\n$v2" | sort -V | head -1)
+
+    case $op in
+        "==")
+            [ "$v1" = "$v2" ]
+            return
+            ;;
+        ">")
+            [ "$v1" != "$v2" ] && [ "$result" = "$v2" ]
+            return
+            ;;
+        "<")
+            [ "$v1" != "$v2" ] && [ "$result" = "$v1" ]
+            return
+            ;;
+        ">=")
+            [ "$result" = "$v2" ]
+            return
+            ;;
+        "<=")
+            [ "$result" = "$v1" ]
+            return
+            ;;
+        *)
+            die $LINENO "unrecognised op: $op"
+            ;;
+    esac
+}
+
+function main {
+    local min_python_version="3.5"
+    local min_pip_version="20"
+
+    if ! command -v python  || _vercmp "$(python -V 2>&1 | awk '{print $2}')" '<' "$min_python_version"; then
         # shellcheck disable=SC1091
         source /etc/os-release || source /usr/lib/os-release
         case ${ID,,} in
+            *suse*)
+                sudo -H -E zypper -q addrepo https://download.opensuse.org/repositories/openSUSE:Leap:15.1:Update/standard/openSUSE:Leap:15.1:Update.repo
+                sudo zypper --gpg-auto-import-keys refresh
+                sudo -H -E zypper -q install -y --no-recommends python3
+            ;;
             ubuntu|debian)
-            sudo -H -E apt-get -y -q=3 install python-minimal
+                sudo -H -E apt-get -y -q=3 install software-properties-common
+                sudo -H -E add-apt-repository -y ppa:deadsnakes/ppa
+                sudo apt update
+                sudo -H -E apt-get -y -q=3 install python3.7
             ;;
             rhel|centos|fedora)
-            PKG_MANAGER=$(command -v dnf || command -v yum)
-            sudo -H -E "${PKG_MANAGER}" -q -y install python36
-            sudo rm /usr/bin/python
-            sudo ln -s /usr/bin/python3 /usr/bin/python
-            sudo sed -i "s|#!/usr/bin/python|#!$(command -v python2)|g" /usr/bin/yum
+                PKG_MANAGER=$(command -v dnf || command -v yum)
+                sudo -H -E "${PKG_MANAGER}" -q -y install python36 yum-utils
+                for file in yum yum-config-manager; do
+                    sudo sed -i "s|#\!/usr/bin/python|#!$(command -v python2)|g" "/usr/bin/$file"
+                done
+                sudo sed -i "s|#\! /usr/bin/python|#!$(command -v python2)|g" /usr/libexec/urlgrabber-ext-down
             ;;
         esac
+        sudo rm -f /usr/bin/python
+        sudo ln -s /usr/bin/python3 /usr/bin/python
     fi
-    curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
+    if ! command -v pip || _vercmp "$(pip -V | awk '{print $2}')" '<' "$min_pip_version"; then
+        curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
+    fi
 }
 
 main
