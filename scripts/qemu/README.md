fac2e7abe81f16082790cf64bc343f266c01bc9c
Victor Morales <electrocucaracha@gmail.com>
2020-05-11 10:40:43 -0700

    Add qemu installation script

---
+++ scripts/qemu/README.md
@@ -0,0 +1,10 @@
+# QEMU
+
+QEMU is a hosted virtual machine monitor: it emulates the machine's
+processor through dynamic binary translation and provides a set of
+different hardware and device models for the machine, enabling it to
+run a variety of guest operating systems. It also can be used with KVM
+to run virtual machines at near-native speed (by taking advantage of
+hardware extensions such as Intel VT-x). QEMU can also do emulation
+for user-level processes, allowing applications compiled for one
+architecture to run on another.
---
+++ scripts/qemu/main.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2020
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o errexit
+set -o pipefail
+if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+    set -o xtrace
+fi
+
+function main {
+    local version=${PKG_QEMU_VERSION:-5.0.0}
+    local qemu_tarball="qemu-${version}.tar.xz"
+    local pmdk_version=${PKG_PMDK_VERSION:-1.4}
+    local pmdk_url="https://github.com/pmem/pmdk/releases/download/$pmdk_version/pmdk-${pmdk_version}-"
+
+    cpu_arch="amd64"
+    if command -v dpkg; then
+        cpu_arch=$(dpkg --print-architecture)
+    fi
+    if [ -n "${PKG_CPU_ARCH:-}" ]; then
+        cpu_arch="$PKG_CPU_ARCH"
+    fi
+
+    configure_flags="--target-list="$(lscpu | grep "Architecture:" | awk '{print $2}')-softmmu" --enable-libpmem --enable-kvm"
+    pkgs="pip gcc make"
+    # shellcheck disable=SC1091
+    source /etc/os-release || source /usr/lib/os-release
+    case ${ID,,} in
+        opensuse*)
+            pkgs+=" bzip2 glib2-devel libpixman-1-0-devel"
+            pushd "$(mktemp -d)" > /dev/null
+            if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+                curl -L -o pmdk-rpms.tar.gz "${pmdk_url}rpms.tar.gz"
+                tar xvf pmdk-rpms.tar.gz
+            else
+                curl -L -o pmdk-rpms.tar.gz "${pmdk_url}rpms.tar.gz" 2> /dev/null
+                tar xf pmdk-rpms.tar.gz
+            fi
+            for pkg in libpmem libpmem-devel; do
+                sudo rpm -i "x86_64/${pkg}-${pmdk_version}-1.fc25.x86_64.rpm"
+            done
+            popd
+        ;;
+        clearlinux)
+            curl -fsSL http://bit.ly/install_pkg | PKG="kvm-host" bash
+            return
+        ;;
+        ubuntu|debian|raspbian)
+            configure_flags+=" --enable-numa"
+            pkgs+=" libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev libpmem-dev libnuma-dev"
+            if [ "$VERSION_CODENAME" == "xenial" ]; then
+                pushd "$(mktemp -d)" > /dev/null
+                if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+                    curl -L -o pmdk-dpkgs.tar.gz "${pmdk_url}dpkgs.tar.gz"
+                    tar xvf pmdk-dpkgs.tar.gz
+                else
+                    curl -L -o pmdk-dpkgs.tar.gz "${pmdk_url}dpkgs.tar.gz" 2> /dev/null
+                    tar xf pmdk-dpkgs.tar.gz
+                fi
+                for pkg in libpmem libpmem-dev; do
+                    sudo dpkg -i "${pkg}_${pmdk_version}-1_${cpu_arch}.deb"
+                done
+                popd
+            fi
+        ;;
+        rhel|centos|fedora)
+            configure_flags+=" --enable-numa"
+            pkgs+=" glib2-devel pixman-devel zlib-devel libpmem-devel numactl-devel"
+        ;;
+    esac
+    curl -fsSL http://bit.ly/install_pkg | PKG="$pkgs" bash
+
+    pushd "$(mktemp -d)" > /dev/null
+    if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+        curl -o qemu.tar.tz "https://download.qemu.org/$qemu_tarball"
+        tar xvf qemu.tar.tz
+    else
+        curl -o qemu.tar.tz "https://download.qemu.org/$qemu_tarball" 2> /dev/null
+        tar xf qemu.tar.tz
+    fi
+    pushd "qemu-${version}" > /dev/null
+    eval "./configure $configure_flags"
+    make
+    sudo make install
+    popd > /dev/null
+    popd > /dev/null
+}
+
+main
