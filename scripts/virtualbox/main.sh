fd61270fc8c9604173ab2d749e34623439d6198b
Victor Morales <v.morales@samsung.com>
2020-07-09 17:51:06 -0700

    Fix VirtualBox for CentOS 8 and openSUSE

--- scripts/virtualbox/main.sh
+++ scripts/virtualbox/main.sh
@@ -29,12 +29,24 @@ function main {
     source /etc/os-release || source /usr/lib/os-release
     case ${ID,,} in
         opensuse*)
-            if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
-                sudo curl -o /etc/zypp/repos.d/virtualbox.repo "http://download.virtualbox.org/virtualbox/rpm/opensuse/virtualbox.repo"
+            supported_versions="11.4 12.3 13.1 13.2 15.0 42.1 42.2 42.3"
+            if [[ "$supported_versions" != *"$VERSION_ID"* ]]; then
+                echo "WARN: VirtualBox's repo is not supported in openSUSE $VERSION_ID"
+                INSTALLER_CMD="sudo -H -E zypper "
+                if [[ "${PKG_DEBUG:-false}" == "false" ]]; then
+                    INSTALLER_CMD+="-q "
+                fi
+                sudo zypper --gpg-auto-import-keys refresh
+                $INSTALLER_CMD install -y --no-recommends virtualbox dkms
+                return
             else
-                sudo curl -o /etc/zypp/repos.d/virtualbox.repo "http://download.virtualbox.org/virtualbox/rpm/opensuse/virtualbox.repo" 2> /dev/null
+                if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+                    sudo curl -o /etc/zypp/repos.d/virtualbox.repo "http://download.virtualbox.org/virtualbox/rpm/opensuse/virtualbox.repo"
+                else
+                    sudo curl -o /etc/zypp/repos.d/virtualbox.repo "http://download.virtualbox.org/virtualbox/rpm/opensuse/virtualbox.repo" 2> /dev/null
+                fi
+                sudo rpm --import oracle_vbox.asc
             fi
-            sudo rpm --import oracle_vbox.asc
         ;;
         ubuntu|debian)
             curl -fsSL http://bit.ly/install_pkg | PKG=gnupg bash
@@ -42,16 +54,14 @@ function main {
             curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -
         ;;
         rhel|centos|fedora)
-            PKG_MANAGER=$(command -v dnf || command -v yum)
             if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
                 sudo curl -o /etc/yum.repos.d/virtualbox.repo https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
                 sudo rpm --import --verbose oracle_vbox.asc
-                sudo -E "$PKG_MANAGER" update --assumeyes --verbose
             else
                 sudo curl -o /etc/yum.repos.d/virtualbox.repo https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo 2> /dev/null
                 sudo rpm --import --quiet oracle_vbox.asc
-                sudo -E "$PKG_MANAGER" update --assumeyes 2> /dev/null
             fi
+            sudo "$(command -v dnf || command -v yum)" repolist --assumeyes || true
         ;;
         clear-linux-os)
             echo "WARN: The VirtualBox provider isn't supported by ClearLinux yet."
