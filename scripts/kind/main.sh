5c7de3640485e48e5471e2e1c4c53fab7656e4a7
Victor Morales <v.morales@samsung.com>
2020-08-14 09:56:05 -0700

    Implement get_cpu_arch in kind script

--- scripts/kind/main.sh
+++ scripts/kind/main.sh
@@ -15,25 +15,38 @@ if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
     set -o xtrace
 fi
 
+function get_cpu_arch {
+    if [ -z "${PKG_CPU_ARCH:-}" ]; then
+        case "$(uname -m)" in
+            x86_64)
+                PKG_CPU_ARCH=amd64
+            ;;
+            armv8*)
+                PKG_CPU_ARCH=arm64
+            ;;
+            aarch64*)
+                PKG_CPU_ARCH=arm64
+            ;;
+            armv*)
+                PKG_CPU_ARCH=armv7
+            ;;
+        esac
+    fi
+    echo "$PKG_CPU_ARCH"
+}
+
 function main {
     local version=${PKG_KIND_VERSION:-0.8.1}
 
     if command -v kind; then
         return
     fi
-    echo "INFO: Installing kind..."
 
-    cpu_arch="amd64"
-    if command -v dpkg; then
-        cpu_arch=$(dpkg --print-architecture)
-    fi
-    if [ -n "${PKG_CPU_ARCH:-}" ]; then
-        cpu_arch="$PKG_CPU_ARCH"
-    fi
+    echo "INFO: Installing kind..."
     if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
-        curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v${version}/kind-$(uname)-$cpu_arch"
+        curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v${version}/kind-$(uname)-$(get_cpu_arch)"
     else
-        curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v${version}/kind-$(uname)-$cpu_arch" 2> /dev/null
+        curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v${version}/kind-$(uname)-$(get_cpu_arch)" 2> /dev/null
     fi
     chmod +x ./kind
     sudo mkdir -p  /usr/local/bin/
