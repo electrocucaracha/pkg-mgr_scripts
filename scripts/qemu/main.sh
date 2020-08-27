eda927156c343dc0f3cb707d273483173a60c3de
Victor Morales <electrocucaracha@gmail.com>
2020-08-26 17:33:21 -0700

    Bump qemu version to 5.1.0

--- scripts/qemu/main.sh
+++ scripts/qemu/main.sh
@@ -35,8 +35,46 @@ function get_cpu_arch {
     echo "$PKG_CPU_ARCH"
 }
 
+# _vercmp() - Function that compares two versions
+function _vercmp {
+    local v1=$1
+    local op=$2
+    local v2=$3
+    local result
+
+    # sort the two numbers with sort's "-V" argument.  Based on if v2
+    # swapped places with v1, we can determine ordering.
+    result=$(echo -e "$v1\n$v2" | sort -V | head -1)
+
+    case $op in
+        "==")
+            [ "$v1" = "$v2" ]
+            return
+            ;;
+        ">")
+            [ "$v1" != "$v2" ] && [ "$result" = "$v2" ]
+            return
+            ;;
+        "<")
+            [ "$v1" != "$v2" ] && [ "$result" = "$v1" ]
+            return
+            ;;
+        ">=")
+            [ "$result" = "$v2" ]
+            return
+            ;;
+        "<=")
+            [ "$result" = "$v1" ]
+            return
+            ;;
+        *)
+            die $LINENO "unrecognised op: $op"
+            ;;
+    esac
+}
+
 function main {
-    local version=${PKG_QEMU_VERSION:-5.0.0}
+    local version=${PKG_QEMU_VERSION:-5.1.0}
     local qemu_tarball="qemu-${version}.tar.xz"
     local pmdk_version=${PKG_PMDK_VERSION:-1.4}
     local pmdk_url="https://github.com/pmem/pmdk/releases/download/$pmdk_version/pmdk-${pmdk_version}-"
@@ -46,12 +84,10 @@ function main {
     fi
 
     configure_flags="--target-list="$(uname -m)-softmmu" --enable-libpmem --enable-kvm"
-    pkgs="pip gcc make"
     # shellcheck disable=SC1091
     source /etc/os-release || source /usr/lib/os-release
     case ${ID,,} in
         opensuse*)
-            pkgs+=" bzip2 glib2-devel libpixman-1-0-devel diffutils"
             if ! sudo zypper search --match-exact --installed-only libpmem; then
                 pushd "$(mktemp -d)" > /dev/null
                 if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
@@ -62,19 +98,24 @@ function main {
                     tar xf pmdk-rpms.tar.gz
                 fi
                 for pkg in libpmem libpmem-devel; do
-                    sudo rpm -i "x86_64/${pkg}-${pmdk_version}-1.fc25.x86_64.rpm"
+                    sudo rpm -i "$(uname -m)/${pkg}-${pmdk_version}-1.fc25.$(uname -m).rpm"
                 done
-                popd
+                popd > /dev/null
             fi
+            sudo -H -E zypper install -y --no-recommends git gcc make bzip2 glib2-devel libpixman-1-0-devel diffutils zlib-devel libbz2-devel libopenssl-devel ncurses-devel readline-devel sqlite3 sqlite3-devel tack xz-devel
+            curl https://pyenv.run | bash
+            export PATH="$HOME/.pyenv/bin:$PATH"
+            eval "$(pyenv init -)"
+            pyenv install 3.8.5
+            pyenv global 3.8.5
         ;;
         clearlinux)
-            curl -fsSL http://bit.ly/install_pkg | PKG="kvm-host" bash
+            sudo -H -E swupd bundle-add kvm-host
             return
         ;;
         ubuntu|debian)
             configure_flags+=" --enable-numa"
-            pkgs+=" libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev libpmem-dev libnuma-dev"
-            if [ "$VERSION_CODENAME" == "xenial" ]; then
+            if _vercmp "${VERSION_ID}" '<=' "16.04"; then
                 pushd "$(mktemp -d)" > /dev/null
                 if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
                     curl -L -o pmdk-dpkgs.tar.gz "${pmdk_url}dpkgs.tar.gz"
@@ -86,15 +127,19 @@ function main {
                 for pkg in libpmem libpmem-dev; do
                     sudo dpkg -i "${pkg}_${pmdk_version}-1_$(get_cpu_arch).deb"
                 done
-                popd
+                popd > /dev/null
             fi
+            sudo -H -E apt-get -y install software-properties-common
+            sudo -H -E add-apt-repository -y ppa:deadsnakes/ppa
+            sudo apt-get update
+            sudo -H -E apt-get -y install --no-install-recommends python3.7 gcc make libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev libpmem-dev libnuma-dev
         ;;
         rhel|centos|fedora)
             configure_flags+=" --enable-numa"
-            pkgs+=" glib2-devel pixman-devel zlib-devel libpmem-devel numactl-devel bzip2"
+            PKG_MANAGER=$(command -v dnf || command -v yum)
+            sudo -H -E "${PKG_MANAGER}" -y install python36 gcc make glib2-devel pixman-devel zlib-devel libpmem-devel numactl-devel bzip2
         ;;
     esac
-    curl -fsSL http://bit.ly/install_pkg | PKG="$pkgs" bash
 
     pushd "$(mktemp -d)" > /dev/null
     if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
@@ -105,7 +150,11 @@ function main {
     tar xf qemu.tar.tz
     pushd "qemu-${version}" > /dev/null
     eval "./configure $configure_flags"
-    make > /dev/null
+    if [[ "${PKG_DEBUG:-false}" == "true" ]]; then
+        make
+    else
+        make > /dev/null
+    fi
     sudo make install
     popd > /dev/null
     popd > /dev/null
