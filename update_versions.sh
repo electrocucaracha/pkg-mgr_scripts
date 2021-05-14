097b9b0cc47d6b005494b75eb1dc9b21264d458a
Victor Morales <chipahuac@hotmail.com>
2021-05-14 16:20:00 -0700

    Add update GH action

---
+++ update_versions.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2019
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o pipefail
+if [[ "${DEBUG:-false}" == "true" ]]; then
+    set -o xtrace
+fi
+
+function get_github_latest_release {
+    version=""
+    attempt_counter=0
+    max_attempts=5
+
+    until [ "$version" ]; do
+        url_effective=$(curl -sL -o /dev/null -w '%{url_effective}' "https://github.com/$1/releases/latest")
+        if [ "$url_effective" ]; then
+            version="${url_effective##*/}"
+            break
+        elif [ ${attempt_counter} -eq ${max_attempts} ];then
+            echo "Max attempts reached"
+            exit 1
+        fi
+        attempt_counter=$((attempt_counter+1))
+        sleep 2
+    done
+    echo "${version#v}"
+}
+
+function get_github_latest_tag {
+    version=""
+    attempt_counter=0
+    max_attempts=5
+
+    until [ "$version" ]; do
+        tags="$(curl -s "https://api.github.com/repos/$1/tags")"
+        if [ "$tags" ]; then
+            version="$(echo "$tags" | grep -Po '"name":.*?[^\\]",' | awk -F  "\"" 'NR==1{print $4}')"
+            break
+        elif [ ${attempt_counter} -eq ${max_attempts} ];then
+            echo "Max attempts reached"
+            exit 1
+        fi
+        attempt_counter=$((attempt_counter+1))
+        sleep 2
+    done
+
+    echo "${version#*v}"
+}
+
+rm -f pinned_versions.env
+while IFS= read -r line; do
+    var=$(echo "${line#*\$\{}" | awk -F ':' '{ print $1}')
+    func=$(echo "${line#*\$(}" | awk -F ')' '{ print $1}')
+    echo "export ${var}=$($func)" | tee --append pinned_versions.env
+done < <(grep -r "_VERSION.*get_github_latest" scripts/ | awk -F '=' '{print $2}')
+sort -o pinned_versions.env pinned_versions.env
