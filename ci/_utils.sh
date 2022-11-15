df836377bb6bf36949faffec2a3e9a221d00a8bd
Victor Morales <chipahuac@hotmail.com>
2022-11-14 20:14:38 -0800

    Move print functions

--- ci/_utils.sh
+++ ci/_utils.sh
@@ -17,25 +17,6 @@ fi
 
 source ./ci/_common.sh
 
-function info {
-    _print_msg "INFO" "$1"
-}
-
-function warn {
-    _print_msg "WARN" "$1"
-    echo "::warning::$1"
-}
-
-function error {
-    _print_msg "ERROR" "$1"
-    echo "::error::$1"
-    exit 1
-}
-
-function _print_msg {
-    echo "$(date +%H:%M:%S) - $1: $2"
-}
-
 function exit_trap {
     $vagrant_cmd ssh "$VAGRANT_NAME" -- cat install.log
     $vagrant_cmd ssh "$VAGRANT_NAME" -- cat validate.log
