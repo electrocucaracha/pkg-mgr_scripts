2250013fa9678a70b4e779ec899ca13300af3f8f
Victor Morales <chipahuac@hotmail.com>
2022-12-01 12:16:32 -0800

    Move devcontainer tests

--- test/cni-plugins/test.sh
+++ test/cni-plugins/test.sh
@@ -1,1 +1,115 @@
-../../src/cni-plugins/validate.sh
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2020
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function info {
+    _print_msg "INFO" "$1"
+}
+
+function error {
+    _print_msg "ERROR" "$1"
+    exit 1
+}
+
+function _print_msg {
+    echo "$1: $2"
+}
+
+function _get_version {
+    local version=${PKG_CNI_PLUGINS_VERSION-}
+    attempt_counter=0
+    max_attempts=5
+
+    until [ "$version" ]; do
+        url_effective=$(curl -sL -o /dev/null -w '%{url_effective}' "https://github.com/containernetworking/plugins/releases/latest")
+        if [ "$url_effective" ]; then
+            version="${url_effective##*/}"
+            break
+        elif [ ${attempt_counter} -eq ${max_attempts} ]; then
+            echo "Max attempts reached"
+            exit 1
+        fi
+        attempt_counter=$((attempt_counter + 1))
+        sleep $((attempt_counter * 2))
+    done
+    echo "v${version#*v}"
+}
+
+function _vercmp {
+    local v1=$1
+    local op=$2
+    local v2=$3
+    local result
+
+    # sort the two numbers with sort's "-V" argument.  Based on if v2
+    # swapped places with v1, we can determine ordering.
+    result=$(echo -e "$v1\n$v2" | sort -V | head -1)
+
+    case $op in
+    "==")
+        [ "$v1" = "$v2" ]
+        return
+        ;;
+    ">")
+        [ "$v1" != "$v2" ] && [ "$result" = "$v2" ]
+        return
+        ;;
+    "<")
+        [ "$v1" != "$v2" ] && [ "$result" = "$v1" ]
+        return
+        ;;
+    ">=")
+        [ "$result" = "$v2" ]
+        return
+        ;;
+    "<=")
+        [ "$result" = "$v1" ]
+        return
+        ;;
+    *)
+        echo "unrecognised op: $op"
+        exit 1
+        ;;
+    esac
+}
+
+function validate_plugin {
+    local plugin=$1
+
+    if [ ! -f "${PKG_CNI_PLUGINS_FOLDER:-/opt/containernetworking/plugins}/$plugin" ]; then
+        error "$plugin CNI binary doesn't exist"
+    fi
+    cni_versions_supported="$(CNI_COMMAND=VERSION "${PKG_CNI_PLUGINS_FOLDER:-/opt/containernetworking/plugins}/$plugin")"
+    if [ -z "$cni_versions_supported" ]; then
+        error "$plugin CNI binary doesn't support CNI version command"
+    fi
+    info "$plugin plugin supports $(echo "$cni_versions_supported" | awk -F ":" '{ gsub(/["\[\]}]/,""); gsub(/,/,", "); print $3}') CNI versions"
+    if _vercmp "$cni_version" '<' "0.9.1" && [[ "$("${PKG_CNI_PLUGINS_FOLDER:-/opt/containernetworking/plugins}/$plugin" --help 2>&1)" != *$cni_version* ]]; then
+        error "$plugin CNI binary version is different than expected"
+    fi
+}
+
+info "Validating CNI plugin folder creation..."
+if [ ! -d "${PKG_CNI_PLUGINS_FOLDER:-/opt/containernetworking/plugins}" ]; then
+    error "CNI plugins folder wasn't created"
+fi
+
+info "Validating CNI plugin binaries installation..."
+cni_version="$(_get_version)"
+for plugin in bandwidth bridge dhcp firewall host-device host-local ipvlan loopback macvlan portmap ptp sbr static tuning vlan; do
+    validate_plugin "$plugin"
+done
+
+if [[ ${PKG_CNI_PLUGINS_INSTALL_FLANNEL:-false} == "true" ]]; then
+    validate_plugin flannel
+fi
--- test/kubectl/test.sh
+++ test/kubectl/test.sh
@@ -1,1 +1,82 @@
-../../src/kubectl/validate.sh
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2019
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function info {
+    _print_msg "INFO" "$1"
+}
+
+function warn {
+    _print_msg "WARN" "$1"
+}
+
+function error {
+    _print_msg "ERROR" "$1"
+    exit 1
+}
+
+function _print_msg {
+    echo "$1: $2"
+}
+
+function get_version {
+    version=""
+    attempt_counter=0
+    max_attempts=5
+
+    until [ "$version" ]; do
+        url_effective=$(curl -sL -o /dev/null -w '%{url_effective}' "https://github.com/$1/releases/latest")
+        if [ "$url_effective" ]; then
+            version="${url_effective##*/}"
+            break
+        elif [ ${attempt_counter} -eq ${max_attempts} ]; then
+            echo "Max attempts reached"
+            exit 1
+        fi
+        attempt_counter=$((attempt_counter + 1))
+        sleep $((attempt_counter * 2))
+    done
+    echo "${version#*v}"
+}
+
+for cmd in kubectl kubectl-convert; do
+    info "Validating $cmd installation..."
+    if ! command -v "$cmd"; then
+        error "$cmd command line wasn't installed"
+    fi
+done
+
+info "Validating autocomplete functions"
+if declare -F | grep -q "_kubectl"; then
+    error "Kubectl autocomplete install failed"
+fi
+
+info "Validating krew installation..."
+if ! kubectl plugin list | grep "kubectl-krew"; then
+    error "Krew plugin wasn't installed"
+fi
+
+info "Checking krew version"
+if [ "$(kubectl krew version | grep GitTag | awk '{ print $2}')" != "v${PKG_KREW_VERSION:-$(get_version kubernetes-sigs/krew)}" ]; then
+    error "Krew version installed is different that expected"
+fi
+
+info "Validating finalize_namespace installation..."
+if ! command -v kubectl-finalize_namespace; then
+    warn "finalize_namespace plugin wasn't installed"
+else
+    info "Checking finalize_namespace version"
+    if [ "$(kubectl finalize_namespace -V | awk '{ print $2}')" != "${PKG_FINALIZE_NAMESPACE_VERSION:-$(get_version mattn/kubectl-finalize_namespace)}" ]; then
+        error "finalize_namespace version installed is different that expected"
+    fi
+fi
